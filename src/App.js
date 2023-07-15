3f20bd9bb91f2828b89fe718f8fa907ad3ae476e
Abhishek7487 <dellabhishek7487@gmail.com>
2023-07-15 14:59:29 +0530

    Initialized project with intial data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+const initialData = [
+  {
+    name: "The North Face",
+    description:
+      "Class V Camp Hat Blue Coral/Summit Navy 5-Panel - The North Face",
+    image: "./public/products/cap-1.jpeg",
+    price: 2500,
+  },
+  {
+    name: "Dickies",
+    description: "Albertville Black 5-Panel - Dickies",
+    image: "./public/products/cap-2.jpeg",
+    price: 2070,
+  },
+  {
+    name: "Fjällräven",
+    description: "High Coast Lite Cap Green 5-Panel - Fjällräven",
+    image: "./public/products/cap-3.jpeg",
+    price: 3200,
+  },
+  {
+    name: "Jack Wolfskin",
+    description: "Supplex Canyon Asphalt 5-Panel - Jack Wolfskin",
+    image: "./public/products/cap-4.jpeg",
+    price: 2240,
+  },
+  {
+    name: "Formula One",
+    description:
+      "Red Bull Racing F1 22 Special Edition USA Navy 5-Panel - Formula One",
+    image: "./public/products/cap-5.jpeg",
+    price: 2800,
+  },
+  {
+    name: "Coal",
+    description: "Provo Olive 5-Panel - Coal",
+    image: "./public/products/cap-6.jpeg",
+    price: 3900,
+  },
+  {
+    name: "Abducted",
+    description: "See You Later Ufo Black 5-Panel - Abducted",
+    image: "./public/products/cap-7.jpeg",
+    price: 3900,
+  },
+  {
+    name: "Iconic",
+    description: "Chili Black 5-Panel - Iconic",
+    image: "./public/products/cap-8.jpeg",
+    price: 2300,
+  },
+  {
+    name: "Fox",
+    description: "Know No Bounds Black 5-Panel - Fox",
+    image: "./public/products/cap-9.jpeg",
+    price: 1700,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hat Store</h1>
     </div>
   );
 }
